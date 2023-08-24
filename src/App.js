a12191d9dd8e65171983594809594a481593e897
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-08-24 16:54:12 +0200

    implement the api with movies and add the css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+//8878f2a0
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=8878f2a0";
 
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Pulp fiction");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input placeholder="Search for movies" value="Pulp Fuction" onChange={() => {}} />
+      </div>
     </div>
   );
 }
