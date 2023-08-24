8b47ca4e0debcefdd44a88a64446b9bf33aee5c8
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-08-24 18:24:26 +0200

    implement Movie cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { useEffect } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCart from "./MovieCart";
+import { useState } from "react";
 //8878f2a0
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=8878f2a0";
+const movie1 = {
+  Title: "Pulp Fiction",
+  Year: "1994",
+  imdbID: "tt0110912",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+};
 
 function App() {
+  const [movies, setMovies] = useState([]);
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
+
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -19,8 +31,25 @@ function App() {
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input placeholder="Search for movies" value="Pulp Fuction" onChange={() => {}} />
+        <input
+          placeholder="Search for movies"
+          value="Pulp Fuction"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCart movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
